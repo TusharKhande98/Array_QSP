686a5d812c4b061c985a232b9ea80ed2a9a2e8bc
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-06-19 19:49:13 +0530

    Add files via upload

---
+++ QSP1_CreateArray_FindLength.java
@@ -0,0 +1,9 @@
+public class QSP1_CreateArray_FindLength
+{
+    public static void main(String[] args)
+    {
+        int[] a = {10, 20, 30, 40, 44, 55, 78, 29};
+        System.out.println(a.length);
+
+    }
+}
