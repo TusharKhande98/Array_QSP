313bad800eec3f94b9aad29980080d14ca6e5b1c
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-06-22 20:41:40 +0530

    Add files via upload

---
+++ QSP3_PrintElementsUsingLoop.java
@@ -0,0 +1,12 @@
+public class QSP3_PrintElementsUsingLoop
+{
+    public static void main(String[] args)
+    {
+        int [] a = {1,2,4,5,6,};
+        for (int i=0; i<a.length; i++)
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+}
