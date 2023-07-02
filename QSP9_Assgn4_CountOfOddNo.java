98beff888cb56d9a70ae2e4a0b1d0897298ad098
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-07-02 12:56:09 +0530

    Add files via upload

---
+++ QSP9_Assgn4_CountOfOddNo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class QSP9_Assgn4_CountOfOddNo
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter size of array:- ");
+        int [] a = new int[s.nextInt()];
+        for (int i=0; i<a.length; i++)
+        {
+            System.out.print("Enter value of array element a[" + i + "]:- ");
+            a[i]=s.nextInt();
+        }
+        int count = 0 ;
+
+        System.out.print("[ ");
+        for (int i=0; i<a.length; i++)
+        {
+            System.out.print(a[i] + " ");
+            if (a[i]%2!=0)
+                count++;
+        }
+        System.out.print("]");
+        System.out.println("\nThere are " + count + " odd numbers in this array");
+    }
+}
