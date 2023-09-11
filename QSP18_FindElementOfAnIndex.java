a21faf63988f91c74c18a56471966cab8c9c51b6
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-09-11 19:31:49 +0530

    QSP18_FindElementOfAnIndex.java

--- QSP18_FindElementOfAnIndex.java
+++ QSP18_FindElementOfAnIndex.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class QSP18_FindElementOfAnIndex
 {
     public static void main(String[] args)
-    {
+    { 
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the size of array:- ");
         int [] arr = new int[s.nextInt()];
