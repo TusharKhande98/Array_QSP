1f941b0796d8460ead809250110a6ff6524a214a
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-09-20 22:40:54 +0530

    Add files via upload

---
+++ QSP33_Pen.java
@@ -0,0 +1,15 @@
+// Desnability Code
+public class QSP33_Pen
+{
+    String pName;
+    String pColor;
+    double pPrice;
+
+    public QSP33_Pen(String pName, String pColor, double pPrice)
+    {
+        super();
+        this.pName = pName;
+        this.pColor = pColor;
+        this.pPrice = pPrice;
+    }
+}
